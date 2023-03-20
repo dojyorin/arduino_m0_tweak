7069c2339be397b5a73a236f9931fd7dcc77ed5a
Kazuki Ota <ota_droid@live.jp>
2023-03-20 21:42:36 +0900

    WIP.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # **Arduino M0 Tweak**
+![Actions-Test](https://github.com/dojyorin/arduino_m0_tweak/actions/workflows/test.yaml/badge.svg)
+![Actions-Release](https://github.com/dojyorin/arduino_m0_tweak/actions/workflows/release.yaml/badge.svg)
+
 Arduino M0 and some ATSAMD21 board tweak.
---
+++ src/adc.cpp
@@ -0,0 +1,14 @@
+/**
+* description.
+*/
+void adcSpeed(){
+    ADC->CTRLA.bit.ENABLE = 0;
+    while(ADC->STATUS.bit.SYNCBUSY);
+
+    ADC->CTRLB.reg = ADC_CTRLB_PRESCALER_DIV64 | ADC_CTRLB_RESSEL_10BIT;
+    ADC->AVGCTRL.reg = ADC_AVGCTRL_SAMPLENUM_1 | ADC_AVGCTRL_ADJRES(0);
+    ADC->SAMPCTRL.reg = 0;
+
+    ADC->CTRLA.bit.ENABLE = 1;
+    while(ADC->STATUS.bit.SYNCBUSY);
+}
