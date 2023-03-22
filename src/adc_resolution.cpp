02ed3a9c0b00097301ec2abb6c81c8e8bed5182c
Kazuki Ota <ota_droid@live.jp>
2023-03-23 04:56:14 +0900

    add resolution step.

--- src/adc_resolution.cpp
+++ src/adc_resolution.cpp
@@ -1,10 +1,10 @@
 #include "./arduino_m0_tweak.hpp"
 
 namespace{
-    constexpr auto SR_8BIT = 8;
-    constexpr auto SR_10BIT = 10;
-    constexpr auto SR_12BIT = 12;
-    constexpr auto SR_16BIT = 16;
+    constexpr auto R_8BIT = 8;
+    constexpr auto R_10BIT = 10;
+    constexpr auto R_12BIT = 12;
+    constexpr auto R_16BIT = 16;
     constexpr auto WAIT_0US = 0;
     constexpr auto WAIT_16US = 32;
 }
@@ -20,9 +20,26 @@ void M0TWEAK::ADC::resolution(uint8_t b){
     ADC->CTRLA.bit.ENABLE = 0;
     while(ADC->STATUS.bit.SYNCBUSY);
 
-    ADC->CTRLB.reg = ADC_CTRLB_PRESCALER_DIV64 | ADC_CTRLB_RESSEL_8BIT;
-    ADC->AVGCTRL.reg = ADC_AVGCTRL_SAMPLENUM_1 | ADC_AVGCTRL_ADJRES(0);
-    ADC->SAMPCTRL.reg = WAIT_0US;
+    if(b == R_8BIT){
+        ADC->CTRLB.reg = ADC_CTRLB_PRESCALER_DIV64 | ADC_CTRLB_RESSEL_8BIT;
+        ADC->AVGCTRL.reg = ADC_AVGCTRL_SAMPLENUM_1 | ADC_AVGCTRL_ADJRES(0);
+        ADC->SAMPCTRL.reg = WAIT_0US;
+    }
+    else if(b == R_10BIT){
+        ADC->CTRLB.reg = ADC_CTRLB_PRESCALER_DIV64 | ADC_CTRLB_RESSEL_10BIT;
+        ADC->AVGCTRL.reg = ADC_AVGCTRL_SAMPLENUM_1 | ADC_AVGCTRL_ADJRES(0);
+        ADC->SAMPCTRL.reg = WAIT_0US;
+    }
+    else if(b == R_12BIT){
+        ADC->CTRLB.reg = ADC_CTRLB_PRESCALER_DIV64 | ADC_CTRLB_RESSEL_12BIT;
+        ADC->AVGCTRL.reg = ADC_AVGCTRL_SAMPLENUM_1 | ADC_AVGCTRL_ADJRES(0);
+        ADC->SAMPCTRL.reg = WAIT_0US;
+    }
+    else if(b == R_16BIT){
+        ADC->CTRLB.reg = ADC_CTRLB_PRESCALER_DIV64 | ADC_CTRLB_RESSEL_16BIT;
+        ADC->AVGCTRL.reg = ADC_AVGCTRL_SAMPLENUM_2 | ADC_AVGCTRL_ADJRES(1);
+        ADC->SAMPCTRL.reg = WAIT_16US;
+    }
 
     ADC->CTRLA.bit.ENABLE = 1;
     while(ADC->STATUS.bit.SYNCBUSY);
