cc07d8997bd6399c764950815b679d24dde9e627
Kazuki Ota <ota_droid@live.jp>
2023-03-24 03:31:41 +0900

    fix namespace structure.

--- src/arduino_m0_tweak.hpp
+++ src/arduino_m0_tweak.hpp
@@ -1,8 +1,13 @@
 #pragma once
 
-#include "inttypes.h"
+#ifdef __SAMD21__
+
+#include "stdint.h"
+#include "sam.h"
 
 namespace M0TWEAK{
     void modifyCpuFrequency(uint8_t f);
     void modifyAdcResolution(uint8_t b);
 }
+
+#endif
