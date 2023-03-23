852eb260ddb579cc4fff22328c723932035f7f41
Kazuki Ota <ota_droid@live.jp>
2023-03-23 11:44:45 +0900

    fix method name.

--- examples/fastadc/fastadc.ino
+++ examples/fastadc/fastadc.ino
@@ -2,7 +2,7 @@
 #include "arduino_m0_tweak.hpp"
 
 void setup(){
-    M0TWEAK::M0ADC::resolution(12);
+    M0TWEAK::modifyAdcResolution(12);
 }
 
 void loop(){}
--- examples/overclock/overclock.ino
+++ examples/overclock/overclock.ino
@@ -2,7 +2,7 @@
 #include "arduino_m0_tweak.hpp"
 
 void setup(){
-    M0TWEAK::M0CPU::frequency(72);
+    M0TWEAK::modifyCpuFrequency(72);
 }
 
 void loop(){}
