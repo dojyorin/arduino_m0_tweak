27eec405f12796ea44d61f6915ab579910ffc267
Kazuki Ota <ota_droid@live.jp>
2023-04-08 21:08:39 +0900

    add descriptions.

--- src/arduino_m0_tweak.hpp
+++ src/arduino_m0_tweak.hpp
@@ -5,13 +5,25 @@
 #include "USB/USBAPI.h"
 
 namespace M0TWEAK{
+    /**
+    * Adjust CPU behavior.
+    */
     namespace M0CPU{
         /**
+        * Change the operating frequency of CPU.
+        * Configurable range is `16` ~ `96` MHz in `1` MHz steps.
+        * Executing this function changes the clock source configuration.
+        * Be sure to read the README.
+        * **Caution! Overclock at your own risk!**
         * @param n CPU Frequency (MHz)
+        * @see https://github.com/dojyorin/arduino_m0_tweak
         */
         void frequency(uint8_t n);
     }
 
+    /**
+    * Adjust ADC behavior.
+    */
     namespace M0ADC{
         /**
         * @param n Sampling resolution (bits)
