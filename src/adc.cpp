3f062a472cd18df71f5d58cd2ae8ca964b92c24f
Kazuki Ota <ota_droid@live.jp>
2023-05-17 03:55:34 +0900

    Improve reg operation.

--- src/adc.cpp
+++ src/adc.cpp
@@ -10,14 +10,14 @@ namespace{
         while(ADC->STATUS.bit.SYNCBUSY);
     }
 
-    void configurePrecision(uint16_t resolution, uint8_t sample, uint8_t duration){
+    void sampleMethod(uint16_t resolution, uint8_t sample, uint8_t wait, uint8_t coefficient){
         ADC->CTRLB.reg = ADC_CTRLB_PRESCALER_DIV64 | resolution;
-        ADC->AVGCTRL.reg = sample | ADC_AVGCTRL_ADJRES(0);
-        ADC->SAMPCTRL.reg = ADC_SAMPCTRL_SAMPLEN(duration);
+        ADC->AVGCTRL.reg = sample | ADC_AVGCTRL_ADJRES(coefficient);
+        ADC->SAMPCTRL.reg = ADC_SAMPCTRL_SAMPLEN(wait);
     }
 }
 
-void m0tweak::m0adc::setResolution(uint8_t n){
+void m0tweak::adcPrecision(uint8_t n){
     if(n != RES_8BIT && n != RES_10BIT && n != RES_12BIT && n != RES_16BIT){
         return;
     }
@@ -26,16 +26,16 @@ void m0tweak::m0adc::setResolution(uint8_t n){
     syncBusy();
 
     if(n == RES_8BIT){
-        configurePrecision(ADC_CTRLB_RESSEL_8BIT, ADC_AVGCTRL_SAMPLENUM_1, 0);
+        sampleMethod(ADC_CTRLB_RESSEL_8BIT, ADC_AVGCTRL_SAMPLENUM_1, 0, 0);
     }
     else if(n == RES_10BIT){
-        configurePrecision(ADC_CTRLB_RESSEL_10BIT, ADC_AVGCTRL_SAMPLENUM_1, 0);
+        sampleMethod(ADC_CTRLB_RESSEL_10BIT, ADC_AVGCTRL_SAMPLENUM_1, 0, 0);
     }
     else if(n == RES_12BIT){
-        configurePrecision(ADC_CTRLB_RESSEL_12BIT, ADC_AVGCTRL_SAMPLENUM_1, 0);
+        sampleMethod(ADC_CTRLB_RESSEL_12BIT, ADC_AVGCTRL_SAMPLENUM_1, 0, 0);
     }
     else if(n == RES_16BIT){
-        configurePrecision(ADC_CTRLB_RESSEL_16BIT, ADC_AVGCTRL_SAMPLENUM_2, 32);
+        sampleMethod(ADC_CTRLB_RESSEL_16BIT, ADC_AVGCTRL_SAMPLENUM_2, 32, 1);
     }
 
     ADC->CTRLA.reg = ADC_CTRLA_ENABLE;
--- src/cpu.cpp
+++ src/cpu.cpp
@@ -8,44 +8,42 @@ namespace{
         while(GCLK->STATUS.bit.SYNCBUSY);
     }
 
-    void activateGenerator(uint8_t id, uint8_t divide, uint32_t source){
+    void inputGCLK(uint8_t id, uint8_t divide, uint32_t source){
         GCLK->GENDIV.reg = GCLK_GENDIV_ID(id) | GCLK_GENDIV_DIV(divide);
         syncBusy();
         GCLK->GENCTRL.reg = GCLK_GENCTRL_ID(id) | source | GCLK_GENCTRL_IDC | GCLK_GENCTRL_GENEN;
         syncBusy();
     }
 
-    void supplyClock(uint32_t target, uint32_t source){
+    void outputGCLK(uint32_t target, uint32_t source){
         GCLK->CLKCTRL.reg = target | source | GCLK_CLKCTRL_CLKEN;
         syncBusy();
     }
 }
 
-void m0tweak::m0cpu::setFrequency(uint8_t f){
+void m0tweak::cpuFrequency(uint8_t f){
     if(f < FREQ_MIN || FREQ_MAX < f){
         return;
     }
 
     SYSCTRL->DPLLRATIO.reg = SYSCTRL_DPLLRATIO_LDR(f - 1);
-    NVMCTRL->CTRLB.bit.RWS = (f + 23) / 24 - 1;
-    // reg = NVMCTRL_CTRLB_RWS()
-
-    if(!SYSCTRL->DPLLSTATUS.bit.ENABLE){
-    SYSCTRL->DPLLCTRLB.reg = SYSCTRL_DPLLCTRLB_FILTER_DEFAULT | SYSCTRL_DPLLCTRLB_REFCLK_GCLK;
-    SYSCTRL->DPLLCTRLA.reg = SYSCTRL_DPLLCTRLA_ENABLE;
-    while(!SYSCTRL->DPLLSTATUS.bit.LOCK & !SYSCTRL->DPLLSTATUS.bit.CLKRDY);
-
-    USBDevice.detach();
-
-    activateGenerator(4, 48, GCLK_GENCTRL_SRC_DFLL48M);
-    activateGenerator(5, 1, GCLK_GENCTRL_SRC_DFLL48M);
-    supplyClock(GCLK_CLKCTRL_ID_FDPLL, GCLK_CLKCTRL_GEN_GCLK4);
-    supplyClock(GCLK_CLKCTRL_ID_USB, GCLK_CLKCTRL_GEN_GCLK5);
-    activateGenerator(0, 1, GCLK_GENCTRL_SRC_FDPLL);
-
-    USBDevice.attach();
-
+    NVMCTRL->CTRLB.reg = NVMCTRL_CTRLB_RWS((f + 23) / 24 - 1);
     // SysTick->LOAD = f * 1000 - 1;
     // SysTick->VAL = 0;
+
+    if(!SYSCTRL->DPLLSTATUS.bit.ENABLE){
+        SYSCTRL->DPLLCTRLB.reg = SYSCTRL_DPLLCTRLB_FILTER_DEFAULT | SYSCTRL_DPLLCTRLB_REFCLK_GCLK;
+        SYSCTRL->DPLLCTRLA.reg = SYSCTRL_DPLLCTRLA_ENABLE;
+        while(!SYSCTRL->DPLLSTATUS.bit.LOCK & !SYSCTRL->DPLLSTATUS.bit.CLKRDY);
+
+        USBDevice.detach();
+
+        inputGCLK(4, 48, GCLK_GENCTRL_SRC_DFLL48M);
+        inputGCLK(5, 1, GCLK_GENCTRL_SRC_DFLL48M);
+        outputGCLK(GCLK_CLKCTRL_ID_FDPLL, GCLK_CLKCTRL_GEN_GCLK4);
+        outputGCLK(GCLK_CLKCTRL_ID_USB, GCLK_CLKCTRL_GEN_GCLK5);
+        inputGCLK(0, 1, GCLK_GENCTRL_SRC_FDPLL);
+
+        USBDevice.attach();
     }
 }
